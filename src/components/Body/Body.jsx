a27ee5c814b76104803053c59535190f825c89cd
Jesper Kling <jesperkling87@gmail.com>
2023-01-06 15:32:27 -0600

    Can now search for songs

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,13 +1,44 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 // components
 import Search from "../Search/Search";
 
-export default function Body() {
+export default function Body({ accessToken, spotifyApi }) {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    if (!spotifyApi) return;
+  }, [accessToken, spotifyApi]);
+
+  useEffect(() => {
+    if (!search) return setResults([]);
+    if (!accessToken) return;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      setResults(
+        res.body.tracks.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: track.album.images[0].url,
+            popularity: track.popularity,
+          };
+        })
+      );
+    });
+  }, [accessToken, search, spotifyApi]);
+
+  console.log(results);
+
   return (
     <div className="ml-24 py-4 space-y-8 md:max-w-6l flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
+      <div className="grid overflow-y-scroll h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4"></div>
     </div>
   );
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 // hooks
 import useAuth from "../../hooks/useAuth";
@@ -7,8 +7,6 @@ import useAuth from "../../hooks/useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
 
 // components
-import TrackSearchResult from "../TrackSearchResult/TrackSearchResult";
-import Player from "../Player/Player";
 import Sidebar from "../Sidebar/Sidebar";
 import Body from "../Body/Body";
 import Right from "../Right/Right";
@@ -19,77 +17,17 @@ const spotifyApi = new SpotifyWebApi({
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
-  const [playingTrack, setPlayingTrack] = useState();
-
-  function chooseTrack(track) {
-    setPlayingTrack(track);
-  }
 
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
 
-  useEffect(() => {
-    if (!search) return setSearchResults([]);
-    if (!accessToken) return;
-
-    let cancel = false;
-    spotifyApi.searchTracks(search).then((res) => {
-      if (cancel) return;
-      setSearchResults(
-        res.body.tracks.items.map((track) => {
-          const smallestAlbumImg = track.album.images.reduce(
-            (smallest, img) => {
-              if (img.height < smallest.height) return img;
-              return smallest;
-            },
-            track.album.images[0]
-          );
-
-          return {
-            artist: track.artists[0].name,
-            title: track.name,
-            uri: track.uri,
-            albumUrl: smallestAlbumImg.url,
-          };
-        })
-      );
-    });
-
-    return () => {
-      cancel = true;
-    };
-  }, [search, accessToken]);
-
   return (
     <div>
-      <form>
-        <input
-          className="w-full border-gray-300 rounded"
-          type="search"
-          placeholder="Search"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
-      <div>
-        {searchResults.map((track) => (
-          <TrackSearchResult
-            track={track}
-            key={track.uri}
-            chooseTrack={chooseTrack}
-          />
-        ))}
-      </div>
       <Sidebar />
-      <Body />
+      <Body accessToken={accessToken} spotifyApi={spotifyApi} />
       <Right />
-      <div>
-        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
-      </div>
     </div>
   );
 }
