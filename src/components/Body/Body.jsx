a95372b13f3a0aafdedd631f0ea0f854b3691908
Jesper Kling <jesperkling87@gmail.com>
2023-01-09 08:45:20 -0600

    Fixed a mistake where new releases wasn't visible from beginning

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import Search from "../Search/Search";
 import Poster from "../Poster/Poster";
 
-export default function Body({ accessToken, spotifyApi }) {
+export default function Body({ accessToken, spotifyApi, chooseTrack }) {
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
   const [newReleases, setNewReleases] = useState([]);
@@ -61,13 +61,25 @@ export default function Body({ accessToken, spotifyApi }) {
     <div className="ml-24 py-4 space-y-8 md:max-w-6l flex-grow md:mr-2.5">
       <Search search={search} setSearch={setSearch} />
       <div className="grid overflow-y-scroll h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 p-4">
-        {newReleases && results.length === 0
+        {results.length === 0
           ? newReleases
               .slice(0, 4)
-              .map((track) => <Poster key={track.id} track={track} />)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))
           : results
               .slice(0, 4)
-              .map((track) => <Poster key={track.id} track={track} />)}
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))}
       </div>
     </div>
   );
