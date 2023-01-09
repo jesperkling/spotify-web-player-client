4461a221550d03a6208ac3ecd1933378f1f839b5
Jesper Kling <jesperkling87@gmail.com>
2023-01-09 08:55:09 -0600

    Can now press play and pause on songs

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -11,12 +11,21 @@ import Sidebar from "../Sidebar/Sidebar";
 import Body from "../Body/Body";
 import Right from "../Right/Right";
 
+// recoil.js
+import { useRecoilState } from "recoil";
+import { playingTrackState } from "../../atoms/playerAtom";
+
 const spotifyApi = new SpotifyWebApi({
   clientId: "2353e1ccf7464652a66074ee88dc47b5",
 });
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
 
   useEffect(() => {
     if (!accessToken) return;
@@ -26,7 +35,11 @@ export default function Dashboard({ code }) {
   return (
     <div>
       <Sidebar />
-      <Body accessToken={accessToken} spotifyApi={spotifyApi} />
+      <Body
+        accessToken={accessToken}
+        spotifyApi={spotifyApi}
+        chooseTrack={chooseTrack}
+      />
       <Right />
     </div>
   );
--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
+import { useRecoilState } from "recoil";
+import { playState, playingTrackState } from "../../atoms/playerAtom";
 
-export default function Poster({ track }) {
+export default function Poster({ track, chooseTrack }) {
+  const [play, setPlay] = useRecoilState(playState);
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const handlePlay = () => {
+    chooseTrack(track);
+
+    if (track.uri === playingTrack.uri) {
+      setPlay(!play);
+    }
+  };
+
   return (
-    <div className="w-[260px] h-[360px] rounded-[50px] overflow-hidden relative cursor-pointer hover:scale-105 transition duration-200 ease-out group mx-auto text-white/80 hover:text-white/100">
+    <div
+      className="w-[260px] h-[360px] rounded-[50px] overflow-hidden relative cursor-pointer hover:scale-105 transition duration-200 ease-out group mx-auto text-white/80 hover:text-white/100"
+      onClick={handlePlay}
+    >
       <img
         src={track.albumUrl}
         alt=""
@@ -11,12 +27,13 @@ export default function Poster({ track }) {
       />
       <div className="absolute bottom-10 inset-x-0 ml-4 flex items-center space-x-3.5">
         <div className="h-10 w-10 bg-[#15883e] rounded-full flex items-center justify-center group-hover:bg-[#1db954] flex-shrink-0">
-          <BsFillPauseFill className="text-xl" />
-          <BsFillPlayFill className="text-xl ml-[1px]" />
+          {track.uri === playingTrack.uri && play ? (
+            <BsFillPauseFill className="text-xl" />
+          ) : (
+            <BsFillPlayFill className="text-xl ml-[1px]" />
+          )}
         </div>
       </div>
     </div>
   );
 }
-
-// 3:36:56 youtube
