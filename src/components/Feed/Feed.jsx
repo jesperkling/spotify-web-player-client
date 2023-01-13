d6b885c7494258c403d3978c9335cf98a6e000b8
Jesper Kling <jesperkling87@gmail.com>
2023-01-13 11:17:50 -0600

    Can no play playlists from feed page in player

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import apiClient from "../../api/Spotify";
 
 export default function Feed() {
@@ -10,13 +11,22 @@ export default function Feed() {
     });
   }, []);
 
+  const navigate = useNavigate();
+
+  const playTopList = (id) => {
+    navigate("/player", { state: { id: id } });
+  };
+
   return (
     <div className="screen-container">
       {topLists?.map((list) => {
         return (
           <div>
-            <img src={list.images[0].url} alt="playlist-art" />
-            <p>{list.name}</p>
+            <img
+              src={list.images[0].url}
+              alt="playlist-art"
+              onClick={() => playTopList(list.id)}
+            />
             <p>{list.description}</p>
           </div>
         );
