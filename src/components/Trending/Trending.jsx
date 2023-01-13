952045ab6685f3a623d8cbd38d69c44d0bf218c0
Jesper Kling <jesperkling87@gmail.com>
2023-01-13 10:23:21 -0600

    Trending page now displays new releases

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import apiClient from "../../api/Spotify";
 
 export default function Trending() {
-  return <div className="screen-container">Trending</div>;
+  const [newReleases, setNewReleases] = useState(null);
+
+  useEffect(() => {
+    apiClient.get("browse/new-releases").then((response) => {
+      setNewReleases(response.data.albums.items);
+      console.log(response.data.albums.items);
+    });
+  }, []);
+
+  console.log("new releases:", newReleases);
+  return (
+    <div className="screen-container">
+      {newReleases?.map((releases) => {
+        return (
+          <div>
+            <img src={releases.images[2].url} alt="album-art" />
+            <p>{releases.name}</p>
+            <p>{releases.artists[0].name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
