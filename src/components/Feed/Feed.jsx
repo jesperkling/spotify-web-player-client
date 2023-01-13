7f94da2527ac71d72c409146087feed1d3fd114d
Jesper Kling <jesperkling87@gmail.com>
2023-01-13 10:55:50 -0600

    Shows top playlists on Feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import apiClient from "../../api/Spotify";
 
 export default function Feed() {
-  return <div className="screen-container">Feed</div>;
+  const [topLists, setTopLists] = useState(null);
+
+  useEffect(() => {
+    apiClient.get("browse/categories/toplists/playlists").then((response) => {
+      setTopLists(response.data.playlists.items);
+    });
+  }, []);
+
+  return (
+    <div className="screen-container">
+      {topLists?.map((list) => {
+        return (
+          <div>
+            <img src={list.images[0].url} alt="playlist-art" />
+            <p>{list.name}</p>
+            <p>{list.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
