48206e713a8421439331e45081ab98ba2c2381d0
Jesper Kling <jesperkling87@gmail.com>
2023-01-13 10:37:24 -0600

    Added top artists & top tracks to favorites page

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -3,10 +3,24 @@ import apiClient from "../../api/Spotify";
 
 export default function Favorites() {
   const [favorites, setFavorites] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     apiClient.get("me/tracks").then((response) => {
       setFavorites(response.data.items);
+    });
+  }, []);
+
+  useEffect(() => {
+    apiClient.get(`me/top/artists`).then((response) => {
+      setTopArtists(response.data.items);
+    });
+  }, []);
+
+  useEffect(() => {
+    apiClient.get(`me/top/tracks`).then((response) => {
+      setTopTracks(response.data.items);
     });
   }, []);
 
@@ -17,6 +31,25 @@ export default function Favorites() {
           <div key={songs.track.id}>
             <img src={songs.track.album.images[2].url} alt="album-art" />
             <p>{songs.track.name}</p>
+          </div>
+        );
+      })}
+
+      {topArtists?.map((artist) => {
+        return (
+          <div key={artist.id}>
+            <img src={artist.images[2].url} alt="artist" />
+            <p>{artist.name}</p>
+          </div>
+        );
+      })}
+
+      {topTracks?.map((track) => {
+        return (
+          <div key={track.id}>
+            <img src={track.album.images[2].url} alt="album-art" />
+            <p>{track.name}</p>
+            <p>{track.artists[0].name}</p>
           </div>
         );
       })}
