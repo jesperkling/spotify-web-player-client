196fa407311e896f153d728befff044fe8a0d689
Jesper Kling <jesperkling87@gmail.com>
2023-01-13 10:13:07 -0600

    Added favorite songs to Favorites component

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import apiClient from "../../api/Spotify";
 
 export default function Favorites() {
-  return <div className="screen-container">Favorites</div>;
+  const [favorites, setFavorites] = useState(null);
+
+  useEffect(() => {
+    apiClient.get("me/tracks").then((response) => {
+      setFavorites(response.data.items);
+    });
+  }, []);
+
+  return (
+    <div className="screen-container">
+      {favorites?.map((songs) => {
+        return (
+          <div key={songs.track.id}>
+            <img src={songs.track.album.images[2].url} alt="album-art" />
+            <p>{songs.track.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
