c33beefc5593736be4c5eac4e79b46b2670135d0
Jesper Kling <jesperkling87@gmail.com>
2023-01-14 19:18:53 -0600

    Styled Trending page with tailwind

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -11,13 +11,13 @@ export default function Trending() {
   }, []);
 
   return (
-    <div className="screen-container">
+    <div className="screen-container grid grid-cols-4 gap-4 p-4 overflow-auto">
       {newReleases?.map((releases) => {
         return (
           <div key={releases.id}>
-            <img src={releases.images[2].url} alt="album-art" />
-            <p>{releases.name}</p>
-            <p>{releases.artists[0].name}</p>
+            <img src={releases.images[0].url} alt="album-art" />
+            <p className="text-white">{releases.name}</p>
+            <p className="text-white/50">{releases.artists[0].name}</p>
           </div>
         );
       })}
