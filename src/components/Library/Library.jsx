b4dc1310466c6e47cb691b884fd9696da176b15c
Jesper Kling <jesperkling87@gmail.com>
2023-01-14 19:43:07 -0600

    Changed styling of Library page to tailwind

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { IconContext } from "react-icons";
-import { AiFillPlayCircle } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import APIKit from "../../api/Spotify";
-import "./Library.css";
 
 export default function Library() {
   const [playlists, setPlaylists] = useState(null);
@@ -22,28 +19,17 @@ export default function Library() {
 
   return (
     <div className="screen-container">
-      <div className="library-body">
+      <div className="p-2 grid grid-cols-3 gap-4 overflow-auto">
         {playlists?.map((playlist) => {
           return (
-            <div
-              className="playlist-card"
-              key={playlist.id}
-              onClick={() => playPlaylist(playlist.id)}
-            >
+            <div key={playlist.id} onClick={() => playPlaylist(playlist.id)}>
               <img
                 src={playlist.images[0].url}
-                className="playlist-art"
                 alt="playlist-art"
+                className="cursor-pointer"
               />
-              <p className="playlist-title">{playlist.name}</p>
-              <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
-              <div className="playlist-fade">
-                <IconContext.Provider
-                  value={{ size: "40px", color: "#E99d72" }}
-                >
-                  <AiFillPlayCircle />
-                </IconContext.Provider>
-              </div>
+              <p className="text-white">{playlist.name}</p>
+              <p className="text-white/50">{playlist.tracks.total} Songs</p>
             </div>
           );
         })}
