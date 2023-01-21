6e94fce91a34c3496d326cd92c933d642e3c4839
Jesper Kling <jesperkling87@gmail.com>
2023-01-21 08:33:23 -0500

    Added name to login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ function Login() {
   return (
     <div className="grid h-screen place-items-center">
       <img src={Logo} alt="" className="h-auto w-40" />
+      <h3>Spotify Clone by Jesper Kling</h3>
       <a
         className="text-white no-underline bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         href={loginEndpoint}
