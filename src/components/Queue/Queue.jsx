8e2b53b339d906db1f426aebb66d81f6417270c5
Jesper Kling <jesperkling87@gmail.com>
2023-03-29 00:39:25 +0200

    Had to remove slice because first song in playlist is not visible anymore

--- src/components/Queue/Queue.jsx
+++ src/components/Queue/Queue.jsx
@@ -4,9 +4,8 @@ export default function Queue({ tracks, setCurrentIndex }) {
   return (
     <div className="p-2">
       <div className="p-2 overflow-auto">
-        <p className="text-white font-bold">Up Next</p>
         <div>
-          {tracks?.slice(1).map((track, index) => (
+          {tracks?.map((track, index) => (
             <div
               key={track?.track.id}
               className="cursor-pointer flex items-center"
@@ -15,7 +14,7 @@ export default function Queue({ tracks, setCurrentIndex }) {
                 src={track?.track.album.images[2].url}
                 alt="album-art"
                 className="rounded py-2"
-                onClick={() => setCurrentIndex(index + 1)}
+                onClick={() => setCurrentIndex(index)}
               />
               <div className="flex flex-col ml-2">
                 <p className="text-white/75 font-bold mb-1">
