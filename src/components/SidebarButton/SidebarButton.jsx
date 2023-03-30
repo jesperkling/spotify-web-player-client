5511c69a1d7fc7df5087f1aec096b2b788f356a6
Jesper Kling <jesperkling87@gmail.com>
2023-03-30 19:56:04 +0200

    Fixed styling on sidebar, width of screen and removed unused style

--- src/components/SidebarButton/SidebarButton.jsx
+++ src/components/SidebarButton/SidebarButton.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 export default function SidebarButton(props) {
   return (
     <Link to={props.to}>
-      <div className="text-white py-4">
+      <div className="text-white py-4 flex flex-col items-center justify-center">
         <IconContext.Provider value={{ size: "24px" }}>
           {props.icon}
-          <p className="">{props.title}</p>
+          <p className="ml-2 hidden sm:inline">{props.title}</p>
         </IconContext.Provider>
       </div>
     </Link>
